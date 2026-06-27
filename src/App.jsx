205433b77fd7a5b7eb60cff237fe6294df26264b
Aishi25 <aishiagarwal11@gmail.com>
2026-06-27 14:19:20 -0400

    Position empty-state text in lower body of envelope watermark

--- src/App.jsx
+++ src/App.jsx
@@ -303,14 +303,14 @@ export default function App() {
           )}
 
           {!results && !isScanning && (
-            <div style={{ position: "relative", display: "flex", alignItems: "center", justifyContent: "center", minHeight: "70vh", textAlign: "center", color: "#7a5c4e" }}>
+            <div style={{ position: "relative", minHeight: "70vh", textAlign: "center", color: "#7a5c4e" }}>
               <img
                 src="/favicon.svg"
                 alt=""
                 style={{ position: "absolute", top: "50%", left: "50%", transform: "translate(-50%, -50%)", width: "min(460px, 55%)", maxHeight: "65%", opacity: 0.07, pointerEvents: "none", zIndex: 0 }}
               />
-              <div style={{ position: "relative", zIndex: 1, maxWidth: 380 }}>
-                <div style={{ fontSize: 32, marginBottom: 12 }}>📬</div>
+              <div style={{ position: "absolute", top: "62%", left: "50%", transform: "translateX(-50%)", zIndex: 1, maxWidth: 360, width: "80%" }}>
+                <div style={{ fontSize: 30, marginBottom: 10 }}>📬</div>
                 <div style={{ fontSize: 15, fontWeight: 500, color: "#555" }}>Sign in with Google and scan your inbox to find subscriptions you can cut.</div>
               </div>
             </div>
